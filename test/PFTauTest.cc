14a4430a8a92f7bfd14c2f7ddc11e6ff76d4b595
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2007-06-26 16:45:22 +0000

    *** empty log message ***

--- test/PFTauTest.cc
+++ test/PFTauTest.cc
@@ -72,6 +72,8 @@ void PFTauTest::analyze(const Event& iEvent, const EventSetup& iSetup){
       cout<<"InvariantMass of the Tau "<<iT->getInvariantMass()<<endl;
       cout<<"Vertex of the Tau "<<iT->vz()<<endl;
       cout<<"Charge of the Tau "<<iT->charge()<<endl;
+      cout<<"Em Over Hadron energy "<<iT->getEmOverHadronEnergy()<<endl;
+      cout<<"Max Hadron energy "<<iT->getMaximumHcalTowerEnergy()<<endl;
       cout<<"# PF charged hadr. cand's "<<iT->getSelectedChargedHadrons().size()<<endl;
       cout<<"# PF neutral hadr. cand's "<<iT->getSelectedNeutralHadrons().size()<<endl;
       cout<<"# PF gamma cand's "<<iT->getSelectedGammaCandidates().size()<<endl;
