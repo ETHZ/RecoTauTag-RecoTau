7da4693e7e85d04537002167bd4fb4df50d6ae5b
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2013-04-04 16:58:46 +0000

    merge PFGammas and PFNeutralHadrons close to charged PFCandidate in dR

--- plugins/PFRecoTauChargedHadronFromPFCandidatePlugin.cc
+++ plugins/PFRecoTauChargedHadronFromPFCandidatePlugin.cc
@@ -17,6 +17,7 @@
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
 #include "DataFormats/TauReco/interface/PFRecoTauChargedHadron.h"
 #include "DataFormats/JetReco/interface/PFJet.h"
+#include "DataFormats/Math/interface/deltaR.h"
 
 #include "RecoTauTag/RecoTau/interface/RecoTauCommonUtilities.h"
 #include "RecoTauTag/RecoTau/interface/RecoTauQualityCuts.h"
@@ -48,6 +49,9 @@ class PFRecoTauChargedHadronFromPFCandidatePlugin : public PFRecoTauChargedHadro
   RecoTauQualityCuts* qcuts_;
 
   std::vector<int> inputPdgIds_;  // type of candidates to clusterize
+
+  double dRmergeNeutralHadron_;
+  double dRmergePhoton_;
 };
 
 PFRecoTauChargedHadronFromPFCandidatePlugin::PFRecoTauChargedHadronFromPFCandidatePlugin(const edm::ParameterSet& pset)
@@ -58,7 +62,10 @@ PFRecoTauChargedHadronFromPFCandidatePlugin::PFRecoTauChargedHadronFromPFCandida
   edm::ParameterSet qcuts_pset = pset.getParameterSet("qualityCuts").getParameterSet("signalQualityCuts");
   qcuts_ = new RecoTauQualityCuts(qcuts_pset);
 
-  inputPdgIds_ = pset.getParameter<std::vector<int> >("stripCandidatesParticleIds");
+  inputPdgIds_ = pset.getParameter<std::vector<int> >("chargedHadronCandidatesParticleIds");
+
+  dRmergeNeutralHadron_ = pset.getParameter<double>("dRmergeNeutralHadron");
+  dRmergePhoton_ = pset.getParameter<double>("dRmergePhoton");
 }
   
 PFRecoTauChargedHadronFromPFCandidatePlugin::~PFRecoTauChargedHadronFromPFCandidatePlugin()
@@ -85,9 +92,25 @@ PFRecoTauChargedHadronFromPFCandidatePlugin::return_type PFRecoTauChargedHadronF
     PFRecoTauChargedHadron::PFRecoTauChargedHadronAlgorithm algo = PFRecoTauChargedHadron::kUndefined;
     if ( fabs((*cand)->charge()) > 0.5 ) algo = PFRecoTauChargedHadron::kChargedPFCandidate;
     else algo = PFRecoTauChargedHadron::kPFNeutralHadron;
+
     std::auto_ptr<PFRecoTauChargedHadron> chargedHadron(new PFRecoTauChargedHadron(**cand, algo));
+
     chargedHadron->chargedPFCandidate_ = (*cand);
+
     chargedHadron->positionAtECALEntrance_ = (*cand)->positionAtECALEntrance();
+
+    std::vector<reco::PFCandidatePtr> jetConstituents = jet.getPFConstituents();
+    for ( std::vector<reco::PFCandidatePtr>::const_iterator jetConstituent = jetConstituents.begin();
+	  jetConstituent != jetConstituents.end(); ++jetConstituent ) {
+      reco::PFCandidate::ParticleType jetConstituentType = (*jetConstituent)->particleId();
+      if ( !(jetConstituentType == reco::PFCandidate::h0 || jetConstituentType == reco::PFCandidate::gamma) ) continue;
+      double dR = deltaR((*jetConstituent)->positionAtECALEntrance(), chargedHadron->positionAtECALEntrance_);
+      double dRmerge = -1.;      
+      if      ( jetConstituentType == reco::PFCandidate::h0    ) dRmerge = dRmergeNeutralHadron_;
+      else if ( jetConstituentType == reco::PFCandidate::gamma ) dRmerge = dRmergePhoton_;
+      if ( dR < dRmerge ) chargedHadron->neutralPFCandidates_.push_back(*jetConstituent);
+    }
+
     output.push_back(chargedHadron);
   }
 
