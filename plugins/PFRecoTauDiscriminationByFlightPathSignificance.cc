c01bc4067e3d1590a116d90b82803200fadc2796
Pavel Jez <sha1-5412db20736c393637675ccc5f0b9ea7f3a21499@cern.ch>
2013-05-13 13:38:12 +0000

    removing dependence on TauTagTools objects that depend on PFTau

--- plugins/PFRecoTauDiscriminationByFlightPathSignificance.cc
+++ plugins/PFRecoTauDiscriminationByFlightPathSignificance.cc
@@ -1,5 +1,4 @@
 #include "RecoTauTag/RecoTau/interface/TauDiscriminationProducerBase.h"
-#include "RecoTauTag/TauTagTools/interface/PFTauQualityCutWrapper.h"
 #include "FWCore/Utilities/interface/InputTag.h"
 
 /* class PFRecoTauDiscriminationByFlightPathSignificance
